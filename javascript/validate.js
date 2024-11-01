a582c2edc82ae023a159bb095b923c329bc1b239
Hedieh Sahafi <hedieh.sahafi@yahoo.com>
2024-11-01 21:51:35 +0330

    Add files via upload
    form validation

---
+++ javascript/validate.js
@@ -0,0 +1,34 @@
+function validateFunction () {
+    var name = document.forms['validateForm']['name'].value;
+    var email = document.forms['validateForm']['email'].value;
+    const validName = name !== "";
+    const validEmail = email.includes('@');
+
+    if (validName && validEmail) {
+        alert('دریافت اطلاعات با موفقیت انجام شد');
+    }
+    else {
+        if (!validName) {
+            alert('لطفا نام را وارد کنید');
+        }
+        if (!validEmail) {
+            alert('لطفا ایمیل معتبر وارد کنید');
+        }
+    } 
+
+/*     if (name == "") {
+        alert ('نام را وارد کنید');
+        return false;
+    }
+    else if (!email.includes('@')) {
+        alert ('ایمیل معتبر وارد کنید');
+        return false;
+    }
+    else {
+        alert('دریافت اطلاعات با موفقیت انجام شد.');
+        return true;
+    } */
+}
+
+
+
